7596a7f1b17c899e44dd3cbe0bafc9965d8919a5
ToolzMing <derek246810@gmail.com>
2024-06-15 15:48:17 -0700

    add 92

---
+++ src/Linked_List/92_Reverse_Linked_List_II.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        
+        # Just do a one pass and switch arround the pointers
+
+        if left == right:
+            return head
+        
+        if not head or left == right:
+            return head
+        
+        dummy = ListNode(0, head)
+        prev = dummy
+        
+        for _ in range(left - 1):
+            prev = prev.next
+        
+        current = prev.next
+        
+        for _ in range(right - left):
+            next_node = current.next
+            current.next, next_node.next, prev.next = next_node.next, prev.next, next_node
+
+        return dummy.next
+        
+            
+                
+
+
